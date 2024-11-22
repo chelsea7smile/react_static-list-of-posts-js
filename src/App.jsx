a2ff656257e53aeb7e7474623c30a5add36c1985
Yevhenii Kucherenko <102630213+chelsea7smile@users.noreply.github.com>
2024-11-22 17:31:16 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,23 @@ import usersFromServer from './api/users.json';
 import { PostList } from './components/PostList/PostList';
 
 export const App = () => {
-  function findUser(userId) {
-    return usersFromServer.find(oneUser => userId === oneUser.id);
-  }
+  const findUserById = (userId) => usersFromServer.find(user => user.id === userId);
 
-  const posts = postsFromServer.map(post => ({
-    ...post,
-    user: findUser(post.userId),
-    comments: commentsFromServer.filter(comment => comment.postId === post.id),
-  }));
+  const findCommentsByPostId = (postId) => commentsFromServer.filter(comment => comment.postId === postId);
+
+  const preparePosts = () => {
+    return postsFromServer.map(post => ({
+      ...post,
+      user: findUserById(post.userId),
+      comments: findCommentsByPostId(post.id),
+    }));
+  };
+
+  const posts = preparePosts();
 
   return (
     <section className="App">
       <h1 className="App__title">Static list of posts</h1>
-
       <PostList posts={posts} />
     </section>
   );
